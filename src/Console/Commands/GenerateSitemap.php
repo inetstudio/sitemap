211ea6caa1e9625bd31476d53355c1d50724f41e
Konstantin Bukin <bukin@inetstudio.ru>
2018-01-13 02:24:56 +0300

    Fixes

--- src/Console/Commands/GenerateSitemap.php
+++ src/Console/Commands/GenerateSitemap.php
@@ -38,7 +38,11 @@ class GenerateSitemap extends Command
                 $mapFormat = (isset($map['options']['format'])) ? $map['options']['format'] : 'xml';
                 $mapStyle = (isset($map['options']['style'])) ? $map['options']['style'] : 'sitemap';
                 $limit = (isset($map['options']['limit'])) ? $map['options']['limit'] : 0;
+
                 $except = (isset($map['except'])) ? $map['except'] : [];
+                $except = collect($except)->map(function ($item) {
+                    return ($item !== '/') ? trim($item, '/') : $item;
+                });
 
                 $items = [];
 
@@ -46,20 +50,16 @@ class GenerateSitemap extends Command
                     $items = array_merge($items, $this->getItems($source));
                 }
 
-                $items = array_merge($items, [
+                $items[] = [
                     'loc' => url('/'),
                     'lastmod' => Carbon::now()->toW3cString(),
                     'priority' => '1.0',
                     'freq' => 'daily',
-                ]);
+                ];
 
                 $items = collect($items)->filter(function ($value) use ($except) {
                     foreach ($except as $pattern) {
-                        if ($pattern !== '/') {
-                            $pattern = trim($pattern, '/');
-                        }
-                        
-                        if (Str::is($pattern, $value['loc'])) {
+                        if (Str::is($pattern, trim(parse_url($value['loc'], PHP_URL_PATH), '/'))) {
                             return false;
                         }
                     }
